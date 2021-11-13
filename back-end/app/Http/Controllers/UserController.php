5b07714c6f33fba0745cee4ebf2ac6354074135f
mattia.cuccato <cuccato.2000@gmail.com>
2021-11-13 14:07:40 +0100

    excel report test backend

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -5,20 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
     public function register(request $request){
         $newUserData = json_decode($request->getContent());
-         $newUser = new User();
-         $newUser -> name = $newUserData->name;
-         $newUser -> surname = $newUserData->surname;
-         $newUser -> birthday =$newUserData->birthday;
-         $newUser -> email = $newUserData->email;
-         $newUser -> password = Hash::make($newUserData->password);
+        $newUser = new User();
+        $newUser -> name = $newUserData->name;
+        $newUser -> surname = $newUserData->surname;
+        $newUser -> birthday =$newUserData->birthday;
+        $newUser -> email = $newUserData->email;
+        $newUser -> password = Hash::make($newUserData->password);
  
-         $newUser -> save();
-         
-         return $newUser;
+        $newUser -> save();
+        return $newUser;
     }
 }
