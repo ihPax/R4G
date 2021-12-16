8d4ea701a78640d340acaf95f8eacd104ab2c2f2
Andrea G <andrea.grendene1@gmail.com>
2021-12-16 11:19:37 +0100

    aggiustata posizione form

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -65,7 +65,7 @@
                 </div>
                 
                 <!--EMAIL-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -80,7 +80,7 @@
                 </div>
 
                 <!--PASSWORD-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
--- front-end/src/components/Registration.vue
+++ front-end/src/components/Registration.vue
@@ -65,7 +65,7 @@
                 </div>
 
                 <!--NOME-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -80,7 +80,7 @@
                 </div>
 
                 <!--COGNOME-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -95,7 +95,7 @@
                 </div>
 
                 <!--DATA-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -110,7 +110,7 @@
                 </div>
                 
                 <!--EMAIL-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -125,7 +125,7 @@
                 </div>
 
                 <!--PASSWORD-->
-                <div class="flex flex-row mt-5">
+                <div class="flex flex-row mt-5 justify-between">
                     <div class="flex flex-col">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 m-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
@@ -139,7 +139,7 @@
                     </div>
                 </div>
 
-                <div class="flex flex-row m-auto mt-5">
+                <div class="flex flex-row m-auto mt-5 justify-between">
                     <div class="flex flex-col m-auto mr-3" v-if="validCheck == false">
                         <input type="checkbox" @click="validCheck=true">
                     </div>
