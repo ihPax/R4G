3f7d819f8108a51e9e04aff0f2a97449437201bd
mattia.cuccato <cuccato.2000@gmail.com>
2022-01-24 09:54:29 +0100

    add choose zones

--- front-end/src/components/Modal.vue
+++ front-end/src/components/Modal.vue
@@ -1,10 +1,9 @@
 <template>
-<div class="flex flex-col h-64 overflow-auto ">
-<div v-for="(comune, index) in comuni" :key="index" class="flex flex-row cursor-pointer hover:bg-gray-200 p-3 rounded " > 
-
-<p class=""> {{comune.name}}</p>
-</div>
-</div>
+    <div class="flex flex-col h-64 overflow-auto ">
+        <div v-for="(comune, index) in comuni" :key="index" class="flex flex-row cursor-pointer hover:bg-gray-200 p-3 rounded"> 
+            <p class="" @click="saveZone(comune)"> {{comune.name}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,12 +13,25 @@ export default {
     data(){
         return{
             comuni:[],
+            user:{
+
+            },
+            access:""
         }
     },
     async mounted(){
-        let response = await axios.get("http://localhost:3000/comuni");
+        let response = await axios.get("http://localhost:8000/r4g/zones");
         this.comuni = response.data
-        console.log(this.comuni);
+    },
+    methods:{
+        async saveZone(comune){
+            this.access = localStorage.getItem("AccessEmail");
+            this.user = {zone_id:comune.id};
+            axios.post("http://localhost:8000/r4g/insert-zone/"+this.access,this.user);
+            this.$router.push({
+                name: "calendar"
+            });
+        }
     }
 }
 </script>
