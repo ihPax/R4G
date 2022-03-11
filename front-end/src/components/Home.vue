7bba02258cac6ba1b7dd22fd8abbbe13a80962d0
Andrea G <andrea.grendene1@gmail.com>
2022-03-11 17:30:53 +0100

    Sistemate alcune cose home mobile

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -138,18 +138,15 @@
       <div class="flex flex-col flex-grow lg:border-l-2 border-black">
         <div class="flex flex-row border-black border-b-2 pb-12 justify-center">
           <div v-if="!user.zone_id">
-            <t-modal v-model="showModal" header="Scegli il tuo Comune" close="chiudi">
+            <t-modal v-model="showModal" header="Scegli la tua zona" close="chiudi">
               <Modal @exit="closeModal"></Modal>
             </t-modal>
-            <t-button @click="showModalTrue()" type="button"
-              >Scegli il tuo comune</t-button
-            >
+            <t-button @click="showModalTrue()" type="button">Scegli la tua zona</t-button>
           </div>
           <div v-if="user.zone_id">
             <Calendar :is-expanded="false"></Calendar>
           </div>
         </div>
-        <div class="flex flex-row">GRAFICO</div>
       </div>
       {{ changeBin }}
     </div>
@@ -175,7 +172,7 @@
                 <Loading></Loading>
               </div>
               <div
-                class="flex flex-col bg-blue-400 rounded-2xl h-72"
+                class="flex flex-col bg-blue-400 rounded-2xl h-76"
                 v-if="localBin != ''"
                 :style="`background-color:${color}`"
               >
@@ -250,26 +247,26 @@
                   <div class="flex flex-col font-bold text-white text-xl">
                     {{ bin.day | date }}
                   </div>
+                  <button class="flex justify-end mb-4" @click="deleteBin()">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-12 w-12 rounded-full bg-white p-2"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      />
+                    </svg>
+                  </button>
                 </div>
-                <button class="flex justify-end mb-4 mr-4" @click="deleteBin()">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-12 w-12 rounded-full bg-white p-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                  </svg>
-                </button>
               </div>
               <div>
-                <t-modal v-model="showModal" header="Scegli il tuo Comune" close="chiudi">
+                <t-modal v-model="showModal" header="Scegli la tua zona" close="chiudi">
                   <Modal @exit="closeModal"></Modal>
                 </t-modal>
               </div>
@@ -279,7 +276,7 @@
             <div class="flex flex-col p-4">
               <div>
                 <div
-                  class="flex flex-col bg-blue-400 rounded-2xl h-72"
+                  class="flex flex-col bg-blue-400 rounded-2xl h-76"
                   :style="`background-color:#166534`"
                 >
                   <div class="flex flex-col px-4 pt-3 justify-center w-full">
@@ -331,31 +328,30 @@
                     <div class="flex flex-col font-bold text-white text-xl">
                       Mercoledì 16 Marzo 2022
                     </div>
+                    <button class="flex justify-end mb-4">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-12 w-12 rounded-full bg-white p-2"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                    </button>
                   </div>
-                  <!--delete bin-->
-                  <button class="flex justify-end mb-4 mr-4">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-12 w-12 rounded-full bg-white p-2"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                      />
-                    </svg>
-                  </button>
                 </div>
               </div>
             </div>
 
             <!-- terza card per delete cestino -->
             <div class="flex flex-col p-4">
-              <div class="w-full flex justify-center items-center h-72 bg-blue-400 rounded-2xl">
+              <div class="w-full flex justify-center items-center h-801 bg-blue-400 rounded-2xl h-76">
                 <button @click="changeBinStatus()" class="h-24 w-24">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-full h-full mx-auto rounded-full bg-gray-200 p-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
@@ -398,10 +394,8 @@ import Modal from "@/components/Modal";
 import Calendar from "@/components/Calendar";
 import ModalMaterial from "@/components/ModalMaterial";
 import Loading from "@/components/Loading";
-import axios from "axios";
 import VueSlickCarousel from "slick-vuejs";
 import "slick-vuejs/dist/slick-vuejs.css";
-
 //import SwichCard from "@/components/SwichCard";
 
 export default {
@@ -426,7 +420,7 @@ export default {
       changeBin: "",
       access: "",
       user: {},
-      rctProva: 550.27,
+      rctProva: 564,
       color: "grey",
       showModal: false,
       valueProva:60,
@@ -465,31 +459,32 @@ export default {
     //calcola la distanza rilevata dal sensore
     getDistance() {
       this.idInterval = setInterval(async () => {
-        this.user = JSON.parse(localStorage.getItem("AccessEmail"));
-        let length = this.userBin[0].length ;
-        let arrayFeeds = await axios.get(
-          "https://api.thingspeak.com/channels/1662872/feeds.json?api_key=HIH5TLATNEAHP71F&results=2"
-        );
-        let lastElement = arrayFeeds.data.feeds.pop();
-        let distance = lastElement.field1;
-        let valore = Math.round(((length - distance) * 100) / length);
-        this.value = isNaN(valore) ? 0 : valore;
+        try {
+          this.user = JSON.parse(localStorage.getItem("AccessEmail"));
+          let length = this.userBin[0].length;
+          let arrayFeeds = await this.$axios.get(
+            "https://api.thingspeak.com/channels/1662872/feeds.json?api_key=HIH5TLATNEAHP71F&results=2"
+          );
+          let lastElement = arrayFeeds.data.feeds.pop();
+          let distance = lastElement.field1;
+          this.value = Math.round(((length - distance) * 100) / length);
 
-        if (this.value > 100) {
-          this.value = 100;
+          if (this.value < 0 || isNaN(this.value)) {
+            this.value = 0;
+          } else if (this.value > 100) {
+            this.value = 100;
+          }
+
+          if (this.value > 80) {
+            await this.$axios.get("/r4g/send-email-percent/" + this.userBin[0].id); //invio email
+          } else if (this.value <= 80) {
+            await this.$axios.put("/r4g/not-send-email-percent/" + this.userBin[0].id);
+          }
+
+          this.changePercent();
+        } catch (e) {
+        this.$emit("catch-error", e);
         }
-
-        if (this.value < 0){
-          this.value = 0;
-        }
-
-        if (this.value > 80) {
-          await axios.get("/r4g/send-email-percent/" + this.userBin[0].id); //invio email
-        } else if (this.value <= 80) {
-          await axios.put("/r4g/not-send-email-percent/" + this.userBin[0].id);
-        }
-
-        this.changePercent();
       },[15000]);
     },
 
@@ -501,9 +496,8 @@ export default {
             "Prima di collegare il cestino ti chiediamo di indicare il tuo quartiere di residenza",
           type: "warning",
           timer: 3000,
-        }).then((r) => {
+        }).then(() => {
           this.showModal = !this.showModal;
-          console.log(r); //NON E' DA TOGLIERE
         });
       } 
       else if (this.viewBinUser.bin_id && this.user.zone_id) {
@@ -534,11 +528,9 @@ export default {
     async getBin() {
       this.isLoading = true;
       try {
-        let response = await this.$axios.get("/r4g/view-bin-user/" + this.user.id);
-        this.viewBinUser = response.data; //bin con userId e binId
+        this.viewBinUser = (await this.$axios.get("/r4g/view-bin-user/" + this.user.id)).data; //bin con userId e binId
 
         if (this.viewBinUser != []) {
-         
           this.binExist = true;
           let bin = await this.$axios.get("/r4g/bin/" + this.user.id);
           let userBin = bin.data; //statistiche bin
@@ -551,7 +543,7 @@ export default {
             this.changePercent();
             this.getDistance();
             let res = await this.$axios.get("/r4g/material-bin/" + this.viewBinUser.bin_id);
-            if (response) {
+            if (res) {
               let calendaBin = res.data;
               this.localBin = JSON.stringify(calendaBin);
               localStorage.setItem("Bin", this.localBin);
@@ -626,8 +618,6 @@ export default {
 
       let c2 = Math.PI * (this.rMobile * 2);
       this.rctMobile = ((100 - this.value) / 100) * c2;
-
-      
     },
 
     //metodo per eliminare il cestino
