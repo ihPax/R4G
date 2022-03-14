70264a44851ce2af70ad2ff38862af6073314b86
zannazanna <zanoni492@gmail.com>
2022-03-14 12:39:02 +0100

    twilwind

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: {
     content: [
-      './public/*.html'
+      './public/index.html'
     ]
   },
   darkMode: 'class', // or 'media' or 'class'
