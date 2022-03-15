9111d0c45f58b1ceec29e98c86ed8b863a5712de
Andrea Grendene <79163309+grendene1@users.noreply.github.com>
2022-03-16 00:41:41 +0100

    In calendario corretto errore da stringa in bool

--- front-end/src/components/Calendar.vue
+++ front-end/src/components/Calendar.vue
@@ -12,7 +12,7 @@
       :min-date="new Date()"
     >
       <template v-slot:day-content="{ day, attributes }">
-        <div class="flex flex-col h-full z-10 overflow-hidden" @click="isMobile ? switchColorLegend('true') : null" :class="isMobile ? 'cursor-pointer' : null">
+        <div class="flex flex-col h-full z-10 overflow-hidden" @click="isMobile ? switchColorLegend(true) : null" :class="isMobile ? 'cursor-pointer' : null">
           <span 
             class="day-label text-gray-900 sm:my-1"
             :class="isExpanded ? 'text-base' : 'text-sm'"
@@ -34,12 +34,12 @@
     </v-calendar>
     <div v-if="isExpanded" class="flex flex-col items-center xs:mt-4 mb-20 xs:mb-4">
       <t-modal v-model="showModal" header="Scegli la tua zona" close="chiudi">
-        <Modal @exit="switchModal('false')" @catch-error="catchErr"></Modal>
+        <Modal @exit="switchModal(false)" @catch-error="catchErr"></Modal>
       </t-modal>
-      <t-button @click="switchModal('true')" type="button">Cambia la zona</t-button>
+      <t-button @click="switchModal(true)" type="button">Cambia la zona</t-button>
     </div>
     <t-modal v-model="showColorLegend" header="Legenda colori" close="chiudi">
-      <ColorLegend @exit="switchColorLegend('false')" @catch-error="catchErr"></ColorLegend>
+      <ColorLegend @exit="switchColorLegend(false)" @catch-error="catchErr"></ColorLegend>
     </t-modal>
   </div>
 </template>
@@ -105,7 +105,7 @@ export default {
      */
     switchModal(isOpening) {
       this.showModal = !this.showModal;
-      if (isOpening === false) {
+      if (isOpening == false) {
         this.attributes = [];
         this.getZone();
       }
