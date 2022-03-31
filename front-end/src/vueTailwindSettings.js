a2ab66b647e53d961a4547c49bfe385502eea4fb
Andrea G <andrea.grendene1@gmail.com>
2022-03-31 17:02:00 +0200

    In calendario, se ultimo giorno del mese, viene evidenziata data ritiro del giorno dopo, quindi il primo giorno del mese dopo. Modifiche minori finestra modale: dato che non funziona il click sull'overlay per uscire dalla finestra, nonostante dovrebbe esserlo di predefinito, ingrandito ed evidenziato l'icona ed il bottone relativo

--- front-end/src/vueTailwindSettings.js
+++ front-end/src/vueTailwindSettings.js
@@ -17,42 +17,42 @@ const vueTailwindSettings = {
       component: TModal,
         props: {
             fixedClasses: {
-                overlay: 'z-40 left-0 top-0 bottom-0 right-0 w-full h-full fixed bg-opacity-50',
+                overlay: 'z-40 left-0 top-0 bottom-0 right-0 w-full h-full fixed bg-opacity-60',
                 wrapper: 'relative mx-auto z-50 max-w-lg px-3 py-6 xs:py-12 justify-center items-center flex',
-                    modal: 'overflow-visible relative rounded',
-                    body: 'p-3',
-                        header: 'border-b border-black p-3 rounded-t font-bold',
-                        footer: ' p-3 rounded-b',
-                            close: 'flex items-center justify-center rounded-full absolute right-0 top-0 -m-3 h-8 w-8 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50'
+                modal: 'overflow-visible relative rounded',
+                body: 'p-3',
+                header: 'border-b border-black p-3 rounded-t font-bold',
+                footer: 'p-3 rounded-b',
+                close: 'flex items-center justify-center rounded-full absolute right-0 top-0 -m-3 w-10 h-10 transition duration-100 ease-in-out focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50'
             },
             classes: {
                 overlay: 'bg-black',
                 wrapper: 'w-screen h-full',
-                    modal: 'bg-white w-9/10',
-                    body: 'p-3',
-                        header: 'border-gray-100',
-                        footer: 'bg-gray-100',
-                            close: 'bg-gray-100 text-gray-600 hover:bg-gray-200',
-                            closeIcon: 'fill-current h-4 w-4',
-                                overlayEnterClass: 'opacity-0',
-                                overlayEnterActiveClass: 'transition ease-out duration-100',
-                                    overlayEnterToClass: 'opacity-100',
-                                    overlayLeaveClass: 'opacity-100',
-                                        overlayLeaveActiveClass: 'transition ease-in duration-75',
-                                        overlayLeaveToClass: 'opacity-0',
-                                            enterClass: '',
-                                            enterActiveClass: '',
-                                                enterToClass: '',
-                                                leaveClass: '',
-                                                    leaveActiveClass: '',
-                                                    leaveToClass: ''
+                modal: 'bg-white w-9/10',
+                body: 'p-3',
+                header: 'border-gray-100',
+                footer: 'bg-gray-100',
+                close: 'bg-gray-200 text-gray-700 hover:bg-gray-300 border border-gray-700',
+                closeIcon: 'fill-current h-6 w-6',
+                overlayEnterClass: 'opacity-0',
+                overlayEnterActiveClass: 'transition ease-out duration-100',
+                overlayEnterToClass: 'opacity-100',
+                overlayLeaveClass: 'opacity-100',
+                overlayLeaveActiveClass: 'transition ease-in duration-75',
+                overlayLeaveToClass: 'opacity-0',
+                enterClass: '',
+                enterActiveClass: '',
+                enterToClass: '',
+                leaveClass: '',
+                leaveActiveClass: '',
+                leaveToClass: ''
             },
             variants: {
                 danger: {
-                overlay: 'bg-red-100',
+                    overlay: 'bg-red-100',
                     header: 'border-red-50 text-red-700',
                     modal: 'bg-white border border-red-100 shadow-lg',
-                        footer: 'bg-red-50'
+                    footer: 'bg-red-50'
                 }
             }
         }
